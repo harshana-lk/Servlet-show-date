a2d97a68bb403ccdd148027add9815af18521183
harshana-lk <mailtoharshanaonlive@gmail.com>
2023-07-11 10:52:52 +0530

    Getting a response using PrintWriter Class

--- src/main/java/me/harshu/servletshowdate/ServletShowDate.java
+++ src/main/java/me/harshu/servletshowdate/ServletShowDate.java
@@ -6,10 +6,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 public class ServletShowDate extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("HI");
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+
+        writer.write("<html lang='en'> <body>");
+        writer.write("<h1>"+new Date()+"</h1>");
+        writer.write("</body> </html>");
     }
 }
